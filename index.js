ee42857a612164b06a2d0b9c637dc54c1b239563
Cxxnd <bwong0227@gmail.com>
2025-11-12 06:02:38 +0700

    requesting commit message for the changes made.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use("/api", (req, res) => {
       "Success access to api"
   });
 });
-app.use("*", (req, res) => {
+app.use("", (req, res) => {
   res.status(404).json({
     success: false,
     message: "api path not found",
